0b102137aa92b4eeef4b57a0f30d1bab26cbbe2e
Nikolay Tsutsarin <leluch.contacts@gmail.com>
2016-06-10 10:46:36 +0300

    Add apt-fast aliases

--- shellrc.d/aliases.d/package-managers.d/apt-fast.sh
+++ shellrc.d/aliases.d/package-managers.d/apt-fast.sh
@@ -1,3 +1,7 @@
-#TODO: check if apt-fast exists
 # apt-fast
-#alias apt-get='apt-fast'
+
+alias afu='apt-fast update'
+alias afi='apt-fast install'
+alias afu='apt-fast upgrade'
+alias afdu='apt-fast dist-upgrade'
+alias afc='apt-fast clean'
