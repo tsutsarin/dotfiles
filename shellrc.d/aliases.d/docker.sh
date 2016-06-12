9da4e43826d4de05e0c72a39a8e92c23cf55533d
Nikolay Tsutsarin <leluch.contacts@gmail.com>
2016-06-12 17:02:08 +0300

    Add remove all docker volumes alias

--- shellrc.d/aliases.d/docker.sh
+++ shellrc.d/aliases.d/docker.sh
@@ -1,13 +1,15 @@
 # docker
 #TODO: could be replaced by ?TOOL
+#TODO: append sudo when detected distro is Ubuntu
+# Commands are Ubuntu-specific because it requires sudo to run docker
 alias doc='sudo docker'
 alias dp='sudo docker pull'
 alias di='sudo docker images'
 alias dc='sudo docker ps -a'
-alias drma=$'sudo docker rm $(sudo docker ps -q -a)'
+alias drmca=$'sudo docker rm $(sudo docker ps -q -a)'
 alias drmia=$'sudo docker rmi $(sudo docker images -q)'
 alias ds='sudo docker search'
-
+alias drmva=$'sudo docker volume rm $(sudo docker volume ls -qf dangling=true)'
 
 
 # add some service aliases
