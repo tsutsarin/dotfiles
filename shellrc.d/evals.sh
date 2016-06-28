8e414385b0e2ce8cc354eff57d459555e9aa78f5
Nikolay Tsutsarin <leluch.contacts@gmail.com>
2016-06-28 19:27:10 +0300

    Add thefuck command

--- shellrc.d/evals.sh
+++ shellrc.d/evals.sh
@@ -1,4 +1,5 @@
 # make less more friendly for non-text input files, see lesspipe(1)
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
 
-# fuck eval
+# thefuck
+eval "$(thefuck --alias)"
