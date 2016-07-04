4740da904c2bcbd16934b56d4a0505b2e4305698
Nikolay Tsutsarin <leluch.contacts@gmail.com>
2016-07-04 23:10:07 +0300

    Add pip aliases

--- shellrc.d/aliases.d/package-managers.d/pip.sh
+++ shellrc.d/aliases.d/package-managers.d/pip.sh
@@ -4,8 +4,10 @@ alias pl='pip list'
 alias psh='pip show'
 #TODO: conflict with default package
 #alias ps='pip search'
+alias pir='sudo pip install -r requirements.txt'
 
 alias pi3='sudo pip3 install'
 alias pl3='pip3 list'
 alias psh3='pip3 show'
 alias ps3='pip3 search'
+alias pir3='sudo pip3 install -r requirements.txt'
