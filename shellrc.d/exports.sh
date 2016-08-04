c2e2fff7082cc533886e32d3a80f07ea16b8bca2
Nikolay Tsutsarin <leluch.contacts@gmail.com>
2016-08-04 18:39:01 +0300

    Set Emacs as default editor

--- shellrc.d/exports.sh
+++ shellrc.d/exports.sh
@@ -1,4 +1,6 @@
-#export EDITOR='vim'
+export ALTERNATE_EDITOR="" # If no Emacs daemon running, start it when connecting
+export EDITOR="emacsclient -t" # $EDITOR should open in terminal
+export VISUAL="emacsclient -c -a emacs" # $VISUAL opens in GUI with non-daemon as alternate
 
 # colored GCC warnings and errors
 export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
