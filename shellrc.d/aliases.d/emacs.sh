ef24156c15258d8d8e9cab65c4d839c7aea471ac
Nikolay Tsutsarin <leluch.contacts@gmail.com>
2016-08-05 02:06:23 +0300

    Add emacsclient wrappers

--- shellrc.d/aliases.d/emacs.sh
+++ shellrc.d/aliases.d/emacs.sh
@@ -1,3 +1,10 @@
-alias emat="emacsclient -t"
-alias semat="sudo emacsclient -t"
-alias emag="emacsclient -c -a emacs" # Opens the GUI with alternate non-daemon
+if command -v ec >/dev/null 2>&1; then # Check if the emacsclient wrapper script exists
+    alias magit='ec -e "(magit-status \"$(pwd)\")"'
+    alias emat='et'
+    alias emag='ec'
+    alias semat='sudo emat'
+else
+    alias emat='emacsclient -t'
+    alias emag='emacsclient -c -a emacs' # Opens the GUI with alternate non-daemon
+    alias semat='sudo emat'
+fi
--- shellrc.d/exports.sh
+++ shellrc.d/exports.sh
@@ -1,6 +1,11 @@
 export ALTERNATE_EDITOR="" # If no Emacs daemon running, start it when connecting
-export EDITOR="emacsclient -t" # $EDITOR should open in terminal
-export VISUAL="emacsclient -c -a emacs" # $VISUAL opens in GUI with non-daemon as alternate
+if command -v ec >/dev/null 2>&1; then # Check if the emacsclient wrapper script exists
+    export EDITOR="et"
+    export VISUAL="ec"
+else
+    export EDITOR="emacsclient -t" # $EDITOR should open in terminal
+    export VISUAL="emacsclient -c -a emacs" # $VISUAL opens in GUI with non-daemon as alternate
+fi
 
 # colored GCC warnings and errors
 export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
