07087b3ef51d273cee48e296cd0729b58d0b3606
Nikolay Tsutsarin <leluch.contacts@gmail.com>
2016-08-10 21:48:36 +0300

    Add additional mknotedir functions

--- shellrc.d/functions.d/mknotedir.sh
+++ shellrc.d/functions.d/mknotedir.sh
@@ -4,3 +4,16 @@ mknotedir()
     mkdir $1 && cd $1
     touch "$1".txt
 }
+
+mknotedirg()
+{
+    mkdir $1 && cd $1
+    touch "$1".txt
+    gedit "$1".txt
+}
+
+mknotedire(){
+    mkdir $1 && cd $1
+    touch "$1".txt
+    $EDITOR "$1".txt
+}
